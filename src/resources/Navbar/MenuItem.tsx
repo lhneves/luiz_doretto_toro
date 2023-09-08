ec8534b7f70434ddb1a5dccf86332390a5c6b6c0
Luiz Doretto <luiz.n.doretto@gmail.com>
2023-09-08 14:02:22 -0300

    feat: create MenuItem and Naviogation components for sidebar on mobile

---
+++ src/resources/Navbar/MenuItem.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+type MenuItemProps = {
+  children: React.ReactNode;
+};
+
+export default function MenuItem({ children }: MenuItemProps) {
+  return <motion.li variants={variants}>{children}</motion.li>;
+}
